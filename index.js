3fb50d253af89756993b8c8485da6840fe4806ee
matteo <matteo.tolazzi@arcadiagroup.co.uk>
2017-01-22 22:39:04 +0000

    Add webhook endpoint for validation

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.get('/', function(req, res) {
     .send('Hi! I am your bot!')
 })
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'montybotverificationtoken') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 app.listen('8080', function() {
   console.log('Bot listening on port 8080')
 })
